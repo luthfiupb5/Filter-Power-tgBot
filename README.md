d02af5c90ee41a5e4137fe8a42fd1833529f711b
luthfiupb5 <73412142+luthfiupb5@users.noreply.github.com>
2020-11-24 16:42:00 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+### One Of My Last BOTs Before Leaving TG💕
+
+
+[![Open Source Love](https://badges.frapsoft.com/os/v1/open-source.png?v=103)](https://github.com/ellerbrock/open-source-badges/)  
+[![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html)
+
+### Click Below Image to Deploy
+[![Deploy](https://coursework.vschool.io/content/images/size/w2000/2018/10/heroku_logo.jpeg)](https://heroku.com/deploy?template=https://github.com/Aid-3n/Advanced-Filter-TG-BOT)
+
+# HELPERS
+*[AIDEN](https://telegram.dog/AID_3N)
+
+*[SARATH](https://telegram.dog/SURV_IVOR)
+
+*[SANDY](https://telegram.dog/MRCONFUSED)
+
+*[POKURT](https://telegram.dog/POKURT)
+
+[Note Do Not Remove These]
---
+++ cinderella/modules/misc.py
@@ -0,0 +1,371 @@
+import html
+import re
+from typing import List, Optional
+import json
+import random
+import time
+import pyowm
+from datetime import datetime
+import os
+import urllib
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
+from bs4 import BeautifulSoup
+
+import requests
+from telegram import Bot, Update, MessageEntity, ParseMode
+from telegram.error import BadRequest
+from telegram.ext import CommandHandler, run_async, Filters
+from telegram.utils.helpers import mention_html
+from telegram import Message
+from telegram import Update, Bot
+from telegram.ext import run_async
+from typing import List
+from telegram import InputMediaPhoto, TelegramError
+
+from cinderella import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, DEV_USERS, WHITELIST_USERS
+from cinderella.__main__ import STATS, USER_INFO, TOKEN
+from cinderella.modules.disable import DisableAbleCommandHandler
+from cinderella.modules.helper_funcs.chat_status import user_admin, sudo_plus
+from cinderella.modules.helper_funcs.extraction import extract_user
+from cinderella import dispatcher, StartTime
+from requests import get
+opener = urllib.request.build_opener()
+useragent = 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36'
+opener.addheaders = [('User-agent', useragent)]
+
+MARKDOWN_HELP = f"""
+Markdown is a very powerful formatting tool supported by telegram. {dispatcher.bot.first_name} has some enhancements, to make sure that \
+saved messages are correctly parsed, and to allow you to create buttons.
+
+- <code>_italic_</code>: wrapping text with '_' will produce italic text
+- <code>*bold*</code>: wrapping text with '*' will produce bold text
+- <code>`code`</code>: wrapping text with '`' will produce monospaced text, also known as 'code'
+- <code>[sometext](someURL)</code>: this will create a link - the message will just show <code>sometext</code>, \
+and tapping on it will open the page at <code>someURL</code>.
+EG: <code>[test](example.com)</code>
+
+- <code>[buttontext](buttonurl:someURL)</code>: this is a special enhancement to allow users to have telegram \
+buttons in their markdown. <code>buttontext</code> will be what is displayed on the button, and <code>someurl</code> \
+will be the url which is opened.
+EG: <code>[This is a button](buttonurl:example.com)</code>
+
+If you want multiple buttons on the same line, use :same, as such:
+<code>[one](buttonurl://example.com)
+[two](buttonurl://google.com:same)</code>
+This will create two buttons on a single line, instead of one button per line.
+
+Keep in mind that your message <b>MUST</b> contain some text other than just a button!
+"""
+
+
+@run_async
+def get_id(bot: Bot, update: Update, args: List[str]):
+    message = update.effective_message
+    chat = update.effective_chat
+    msg = update.effective_message
+    user_id = extract_user(msg, args)
+
+    if user_id:
+
+        if msg.reply_to_message and msg.reply_to_message.forward_from:
+
+            user1 = message.reply_to_message.from_user
+            user2 = message.reply_to_message.forward_from
+
+            msg.reply_text(f"The original sender, {html.escape(user2.first_name)},"
+                           f" has an ID of <code>{user2.id}</code>.\n"
+                           f"The forwarder, {html.escape(user1.first_name)},"
+                           f" has an ID of <code>{user1.id}</code>.",
+                           parse_mode=ParseMode.HTML)
+
+        else:
+
+            user = bot.get_chat(user_id)
+            msg.reply_text(f"{html.escape(user.first_name)}'s id is <code>{user.id}</code>.",
+                           parse_mode=ParseMode.HTML)
+
+    else:
+
+        if chat.type == "private":
+            msg.reply_text(f"Your id is <code>{chat.id}</code>.",
+                           parse_mode=ParseMode.HTML)
+
+        else:
+            msg.reply_text(f"This group's id is <code>{chat.id}</code>.",
+                           parse_mode=ParseMode.HTML)
+
+
+@run_async
+def gifid(bot: Bot, update: Update):
+    msg = update.effective_message
+    if msg.reply_to_message and msg.reply_to_message.animation:
+        update.effective_message.reply_text(f"Gif ID:\n<code>{msg.reply_to_message.animation.file_id}</code>",
+                                            parse_mode=ParseMode.HTML)
+    else:
+        update.effective_message.reply_text("Please reply to a gif to get its ID.")
+
+@run_async
+@user_admin
+def echo(bot: Bot, update: Update):
+    args = update.effective_message.text.split(None, 1)
+    message = update.effective_message
+
+    if message.reply_to_message:
+        message.reply_to_message.reply_text(args[1])
+    else:
+        message.reply_text(args[1], quote=False)
+
+    message.delete()
+
+
+@run_async
+def markdown_help(bot: Bot, update: Update):
+    update.effective_message.reply_text(MARKDOWN_HELP, parse_mode=ParseMode.HTML)
+    update.effective_message.reply_text("Try forwarding the following message to me, and you'll see!")
+    update.effective_message.reply_text("/save test This is a markdown test. _italics_, *bold*, `code`, "
+                                        "[URL](example.com) [button](buttonurl:github.com) "
+                                        "[button2](buttonurl://google.com:same)")
+
+
+@run_async
+@sudo_plus
+def stats(bot: Bot, update: Update):
+    stats = "Current stats:\n" + "\n".join([mod.__stats__() for mod in STATS])
+    result = re.sub(r'(\d+)', r'<code>\1</code>', stats)
+    update.effective_message.reply_text(result, parse_mode=ParseMode.HTML)
+    
+def get_readable_time(seconds: int) -> str:
+
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+        
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+@run_async
+def ping(bot: Bot, update: Update):
+    start_time = time.time()
+    requests.get('https://api.telegram.org')
+    end_time = time.time()
+    ping_time = str(round((end_time - start_time), 2) % 60)
+    uptime = get_readable_time((time.time() - StartTime))
+    update.effective_message.reply_text(f"🏓 Pong!\n⏱️<b>Reply took:</b> {ping_time}s\n🔮<b>Service Uptime:</b> {uptime}", parse_mode=ParseMode.HTML)
+
+@run_async
+def uptime(bot: Bot, update: Update):
+	uptime = get_readable_time((time.time() - StartTime))
+	update.effective_message.reply_text(f"🔮Service Uptime: {uptime}")
+	
+@run_async
+def reverse(bot: Bot, update: Update, args: List[str]):
+    if os.path.isfile("okgoogle.png"):
+        os.remove("okgoogle.png")
+
+    msg = update.effective_message
+    chat_id = update.effective_chat.id
+    rtmid = msg.message_id
+    imagename = "okgoogle.png"
+
+    reply = msg.reply_to_message
+    if reply:
+        if reply.sticker:
+            file_id = reply.sticker.file_id
+        elif reply.photo:
+            file_id = reply.photo[-1].file_id
+        elif reply.document:
+            file_id = reply.document.file_id
+        else:
+            msg.reply_text("Reply to an image or sticker to lookup.")
+            return
+        image_file = bot.get_file(file_id)
+        image_file.download(imagename)
+        if args:
+            txt = args[0]
+            try:
+                lim = int(txt)
+            except:
+                lim = 2
+        else:
+            lim = 2
+    elif args and not reply:
+        splatargs = msg.text.split(" ")
+        if len(splatargs) == 3:                
+            img_link = splatargs[1]
+            try:
+                lim = int(splatargs[2])
+            except:
+                lim = 2
+        elif len(splatargs) == 2:
+            img_link = splatargs[1]
+            lim = 2
+        else:
+            msg.reply_text("/reverse <link> <amount of images to return.>")
+            return
+        try:
+            urllib.request.urlretrieve(img_link, imagename)
+        except HTTPError as HE:
+            if HE.reason == 'Not Found':
+                msg.reply_text("Image not found.")
+                return
+            elif HE.reason == 'Forbidden':
+                msg.reply_text("Couldn't access the provided link, The website might have blocked accessing to the website by bot or the website does not existed.")
+                return
+        except URLError as UE:
+            msg.reply_text(f"{UE.reason}")
+            return
+        except ValueError as VE:
+            msg.reply_text(f"{VE}\nPlease try again using http or https protocol.")
+            return
+    else:
+        msg.reply_markdown("Please reply to a sticker, or an image to search it!\nDo you know that you can search an image with a link too? `/reverse [picturelink] <amount>`.")
+        return
+
+    try:
+        searchUrl = 'https://www.google.com/searchbyimage/upload'
+        multipart = {'encoded_image': (imagename, open(imagename, 'rb')), 'image_content': ''}
+        response = requests.post(searchUrl, files=multipart, allow_redirects=False)
+        fetchUrl = response.headers['Location']
+
+        if response != 400:
+            xx = bot.send_message(chat_id, "Image was successfully uploaded to Google."
+                                  "\nParsing it, please wait.", reply_to_message_id=rtmid)
+        else:
+            xx = bot.send_message(chat_id, "Google told me to go away.", reply_to_message_id=rtmid)
+            return
+
+        os.remove(imagename)
+        match = ParseSauce(fetchUrl + "&hl=en")
+        guess = match['best_guess']
+        if match['override'] and not match['override'] == '':
+            imgspage = match['override']
+        else:
+            imgspage = match['similar_images']
+
+        if guess and imgspage:
+            xx.edit_text(f"[{guess}]({fetchUrl})\nProcessing...", parse_mode='Markdown', disable_web_page_preview=True)
+        else:
+            xx.edit_text("Couldn't find anything.")
+            return
+
+        images = scam(imgspage, lim)
+        if len(images) == 0:
+            xx.edit_text(f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})"
+                          "\nCouldn't fetch any images.", parse_mode='Markdown', disable_web_page_preview=True)
+            return
+
+        imglinks = []
+        for link in images:
+            lmao = InputMediaPhoto(media=str(link))
+            imglinks.append(lmao)
+
+        bot.send_media_group(chat_id=chat_id, media=imglinks, reply_to_message_id=rtmid)
+        xx.edit_text(f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})", parse_mode='Markdown', disable_web_page_preview=True)
+    except TelegramError as e:
+        print(e)
+    except Exception as exception:
+        print(exception)
+
+def ParseSauce(googleurl):
+    """Parse/Scrape the HTML code for the info we want."""
+
+    source = opener.open(googleurl).read()
+    soup = BeautifulSoup(source, 'html.parser')
+
+    results = {
+        'similar_images': '',
+        'override': '',
+        'best_guess': ''
+    }
+
+    try:
+         for bess in soup.findAll('a', {'class': 'PBorbe'}):
+            url = 'https://www.google.com' + bess.get('href')
+            results['override'] = url
+    except:
+        pass
+
+    for similar_image in soup.findAll('input', {'class': 'gLFyf'}):
+            url = 'https://www.google.com/search?tbm=isch&q=' + urllib.parse.quote_plus(similar_image.get('value'))
+            results['similar_images'] = url
+
+    for best_guess in soup.findAll('div', attrs={'class':'r5a77d'}):
+        results['best_guess'] = best_guess.get_text()
+
+    return results
+
+def scam(imgspage, lim):
+    """Parse/Scrape the HTML code for the info we want."""
+
+    single = opener.open(imgspage).read()
+    decoded = single.decode('utf-8')
+    if int(lim) > 10:
+        lim = 10
+
+    imglinks = []
+    counter = 0
+
+    pattern = r'^,\[\"(.*[.png|.jpg|.jpeg])\",[0-9]+,[0-9]+\]$'
+    oboi = re.findall(pattern, decoded, re.I | re.M)
+
+    for imglink in oboi:
+        counter += 1
+        imglinks.append(imglink)
+        if counter >= int(lim):
+            break
+
+    return imglinks
+
+
+__help__ = """
+ - /id: Get the current group id. If used by replying to a message, gets that user's id.
+ - /info: Get information about a user.
+ - /gifid: Get GiF ID.
+ - /markdownhelp: quick summary of how markdown works in telegram - can only be called in private chats.
+ - /ping : Get My Ping Time
+ - /uptime: Find last service update time
+ - /reverse: Use To Find Same Picture From Google 
+"""
+
+ID_HANDLER = DisableAbleCommandHandler("id", get_id, pass_args=True)
+GIFID_HANDLER = DisableAbleCommandHandler("gifid", gifid)
+ECHO_HANDLER = DisableAbleCommandHandler("echo", echo, filters=Filters.group)
+MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help, filters=Filters.private)
+STATS_HANDLER = CommandHandler("stats", stats)
+PING_HANDLER = DisableAbleCommandHandler("ping", ping)
+UPTIME_HANDLER = DisableAbleCommandHandler("uptime", uptime)
+REVERSE_HANDLER = DisableAbleCommandHandler("reverse", reverse, pass_args=True, admin_ok=True)
+
+dispatcher.add_handler(REVERSE_HANDLER)
+dispatcher.add_handler(UPTIME_HANDLER)
+dispatcher.add_handler(PING_HANDLER)
+
+dispatcher.add_handler(ID_HANDLER)
+dispatcher.add_handler(GIFID_HANDLER)
+dispatcher.add_handler(ECHO_HANDLER)
+dispatcher.add_handler(MD_HELP_HANDLER)
+dispatcher.add_handler(STATS_HANDLER)
+
+__mod_name__ = "Main Module😜"
+__command_list__ = ["id", "echo"]
+__handlers__ = [ID_HANDLER, ECHO_HANDLER, MD_HELP_HANDLER, STATS_HANDLER]
